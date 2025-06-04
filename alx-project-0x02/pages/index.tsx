e7aa963ad9b5ed67a80025671a991c1769e9ce06
godwinLaryea <kalexgun@gmail.com>
2025-06-04 14:24:39 +0000

    added header to main page

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/layout/Header";
+
 export default function Home() {
   return (
   <div>
-    <h1 className="text-center text-4xl font-bold">Welcome to my Next.js Setup and Basics</h1>
+    <Header/>
+
+    <h1 className="text-center text-4xl font-bold">Welcome to my Next.js Setup and Basics Project</h1>
+
   </div>
   );
 }
