422ca88ce202b741c22b4a0cf599c645a2630072
godwinLaryea <kalexgun@gmail.com>
2025-06-04 14:23:24 +0000

    added about page

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,10 @@
+
+const about = () => {
+  return (
+    <section>
+      <h1 className="text-4xl text-center font-bold">Welcome to the about section</h1>
+    </section>
+  )
+}
+
+export default about
