484fca7766056fbe07510aecc61cf90f13a9f511
godwinLaryea <kalexgun@gmail.com>
2025-06-04 16:27:25 +0000

    added Card component

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,24 @@
+import { CardProps } from "@/interfaces";
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+  return (
+    <section className="px-4">
+      <div className="flex gap-4 ">
+        <div className="flex flex-col justify-around h-[300px] w-[320px] p-4 rounded-xl shadow-lg cursor-pointer">
+          <img src="#" alt={`${title} content image`} />
+
+          <div className="space-y-4">
+            <h1 className="text-2xl font-semibold">{title}</h1>
+            <p className="w-[80%] line-clamp-2">{content}</p>
+          </div>
+
+          <button className="text-white bg-sky-400 font-medium w-[60%] px-2 py-1 rounded-lg cursor-pointer hover:underline">
+            <a href="#">Click to read more</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Card;
