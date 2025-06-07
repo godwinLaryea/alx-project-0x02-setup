de4230d8ae30f07a3c5f351c6d7562b6e931c06a
godwinLaryea <kalexgun@gmail.com>
2025-06-07 14:45:59 +0000

    interfaces for components

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,3 +1,4 @@
+import PostCard from "../components/common/PostCard";
 
 export interface CardProps {
   title: string;
@@ -5,12 +6,47 @@ export interface CardProps {
 }
 
 export interface PostProps {
-  id: number;
+  userId: number;
   title: string;
   content: string;
+}
+
+export interface ButtonProps {
+  size: "w-sm" | "w-md" | "w-lg";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  title: "small" | "medium" | "large";
 }
 
 export interface PostModalProps {
   onClose: () => void;
   onSubmit: (post: PostProps) => void;
+}
+
+export interface PostCardProps {
+  userId: number;
+  title: string;
+}
+
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
