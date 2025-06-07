dc8a7869a3ddab5c34e0e3dfede4c32cf8bc8a91
godwinLaryea <kalexgun@gmail.com>
2025-06-07 12:18:30 +0000

    added new post feature to home page

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,21 +1,54 @@
 import Header from "@/components/layout/Header";
 import Card from "@/components/common/Card";
+import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
+import { PostProps, PostModalProps } from "@/interfaces";
+import { useState } from "react";
 
-const home = () => {
+const Home: React.FC<PostProps> = ({ id, title, content }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const [post, setPost] = useState([
+    {
+      id: 1,
+      title: "Why Next JS?",
+      content:
+        "One of the primary benefits of Next. js is its ability to improve search engine optimization (SEO) significantly. Through server-side rendering and static site generation, the framework ensures that content is easily accessible and ready to be indexed by search engines.",
+    },
+  ]);
+
+  const handleAddPost = (newPost: PostProps) => {
+    setPost(prevPosts => [...prevPosts, newPost, ]);
+  };
+
   return (
-    <section>
-      <div>
-        <Header />
-        <div>
-          <Card
-            title="Next.js Setup and Basics"
-            content="This is an introduction to next js setup and basic configurations. 
-            The project covers fundamental concepts from initial setup to building interactive components and fetching data from external APIs."
-          />
+    <main>
+      <Header />
+      <section className="px-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">Post Contents</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-sky-400 font-semibold px-4 py-2 rounded-full text-white cursor-pointer"
+          >
+            Add New Post
+          </button>
         </div>
-      </div>
-    </section>
+        <div className="grid grid-cols-[repeat(auto-fill,_minmax(320px,_1fr))] gap-4">
+          {post.map(({ title, content, id }: PostProps, key: number) => (
+            <PostCard title={title} content={content} id={id} key={key}/>
+          ))}
+        </div>
+      </section>
+
+      {isModalOpen && (
+        <PostModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddPost}
+        />
+      )}
+    </main>
   );
 };
 
-export default home;
+export default Home;
