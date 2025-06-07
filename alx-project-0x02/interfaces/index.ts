efa53fce455eb9e5053d6935cde69ba7279890ac
godwinLaryea <kalexgun@gmail.com>
2025-06-07 12:17:43 +0000

    Added postprops and postmodal props interface

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,4 +1,16 @@
+
 export interface CardProps {
   title: string;
   content: string;
+}
+
+export interface PostProps {
+  id: number;
+  title: string;
+  content: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostProps) => void;
 }
