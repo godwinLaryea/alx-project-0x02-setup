43870e1426bab4fdeb6431f03c3c823e6babed22
godwinLaryea <kalexgun@gmail.com>
2025-06-07 14:44:40 +0000

    added an about page with buttons

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,12 +1,20 @@
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
-const about = () => {
+const About = () => {
   return (
     <section>
-      <Header/>
-      <h1 className="text-4xl text-center font-bold">Welcome to the about section</h1>
+      <Header />
+      <h1 className="text-4xl text-center font-bold">
+        Welcome to the about section
+      </h1>
+      <div className="flex flex-col items-center justify-center gap-2 flex-wrap mt-4">
+        <Button size="w-sm" shape="rounded-sm" title="small"></Button>
+        <Button size="w-md" shape="rounded-md" title="medium"></Button>
+        <Button size="w-lg" shape="rounded-full" title="large"></Button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default about
+export default About;
