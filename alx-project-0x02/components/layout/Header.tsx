9ccce0eb3810ea30588f4fb5d51d472bda82cbbc
godwinLaryea <kalexgun@gmail.com>
2025-06-07 14:41:31 +0000

    added posts and users page links

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -11,6 +11,8 @@ const Header: React.FC = () => {
         <ul className="space-x-4">
           <Link href="/home">Home</Link>
           <Link href="/about">About</Link>
+          <Link href="/posts">Posts</Link>
+          <Link href="/users">Users</Link>
         </ul>
       </nav>
     </header>
